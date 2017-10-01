70ae435558c9a79c816346e523c61c1015e606dd
Ulzee An <ul1994@gmail.com>
2017-10-01 03:41:05 -0400

    moving cv2 import below other deps. because of numpy conflict

--- Pose_Estimation/single_from_videos_to_joints.py
+++ Pose_Estimation/single_from_videos_to_joints.py
@@ -1,4 +1,3 @@
-import cv2
 import time
 from torch import np
 import argparse
@@ -9,7 +8,7 @@ import codecs, json
 
 from Functions import handle_one, df_coordinates, score_coordinates
 import ast
-
+import cv2
 
 parser = argparse.ArgumentParser(description='Pose Estimation Baseball')
 parser.add_argument('input_file', metavar='DIR', # Video file to be processed
@@ -60,6 +59,7 @@ if True: #__name__ == "__main__":
         batter = frame[top_b:bottom_b, left_b:right_b]
         df.loc[p]=[int(p),handle_one(pitcher),handle_one(batter) ]
         p+=1
+        # if p is 2: break
     print("Time to read in video and handle one:", time.time()-tic1)
     #print("nach handle one shape ", df.loc[p-1]["Pitcher"].shape)
     #try:
